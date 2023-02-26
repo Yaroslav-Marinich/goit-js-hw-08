9ed4991a30f76282e54d1c23242870ab9b8773fd
Yaroslav-Marinich <ysmarinich@gmail.com>
2023-02-26 14:56:01 +0200

    add task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+window.onload = event => {
+  if (localStorage.getItem('videoplayer-current-time') !== null) {
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+  }
+};
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    let saveTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', saveTime);
+    console.log(saveTime);
+  }, 1000)
+);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
